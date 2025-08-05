bd05c5ccba588279481712bc3a89634f6b6793ff
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-08-05 17:13:43 +0800

    test: use the logger to check root endpoint health

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from .core.logger import get_logger
+
 app = FastAPI()
+logger = get_logger(__name__)
 
 
 @app.get("/")
 def root():
+    logger.info("Root endpoint hit!")
     return {"message": "Document Tracking API is running!"}
