c769d8c0927445c49f9c4e3899cbddee04e22a74
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-08-06 12:55:44 +0800

    refactor: reuse user dependency injector

--- app/services/admin.py
+++ app/services/admin.py
@@ -1,6 +1,4 @@
-# app/services/admin.py
-
-from typing import Annotated, List, Optional
+from typing import Annotated, List
 
 from fastapi import Depends
 
@@ -13,35 +11,39 @@ from ..utils import exceptions
 
 logger = get_logger(__name__)
 
+# Reusable dependency type
+UserRepoDependency = Annotated[UserRepository, Depends(get_user_repository)]
+
 
 class AdminService:
-    def __init__(self, user_repository: UserRepository):
+    def __init__(self, user_repository: UserRepoDependency):
         self.user_repository = user_repository
 
-    def create_user(self, create_data: UserCreate) -> Optional[User]:
-        exsiting_user = self.user_repository.get_user(create_data.username)
-        if exsiting_user:
-            logger.info(f"User already exists! (username: '{exsiting_user.username}')")
+    def create_user(self, create_data: UserCreate) -> UserRead:
+        existing_user = self.user_repository.get_user(create_data.username)
+        if existing_user:
+            logger.info(f"User creation failed: '{create_data.username}' already exists.")
             raise exceptions.bad_request("User already exists")
 
         hashed_password = hash_password(create_data.password)
         new_user = User(
-            **create_data.model_dump(exclude="password"),
+            **create_data.model_dump(exclude={"password"}),
             hashed_password=hashed_password,
         )
+
         created_user = self.user_repository.create(new_user)
-        logger.info(f"New user created! (username: '{new_user.username}')")
+        logger.info(f"User created successfully: '{new_user.username}'")
         return UserRead.model_validate(created_user)
 
     def get_all_users(self) -> List[UserRead]:
         users = self.user_repository.get_all()
         if not users:
-            logger.info("Users table empty!")
+            logger.info("User fetch failed: No active users found.")
             raise exceptions.bad_request("No active users")
+
         return [UserRead.model_validate(user) for user in users]
 
 
-def get_admin_service(
-    user_repository: Annotated[UserRepository, Depends(get_user_repository)],
-):
+# Dependency injector
+def get_admin_service(user_repository: UserRepoDependency) -> AdminService:
     return AdminService(user_repository)
